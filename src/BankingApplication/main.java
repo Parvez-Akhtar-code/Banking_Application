a4edcaed0efe6d13813413da49ed77753fd87e2c
Parvez Akhtar <parvezakhtar310@gmail.com>
2024-01-08 19:22:31 +0530

    intial commit

--- src/BankingApplication/main.java
+++ src/BankingApplication/main.java
@@ -2,7 +2,7 @@ package BankingApplication;
 
 public class main {
     public static void main(String[] args) {
-        Bank bank = new Bank("Jawan" , "0782");
+        Bank bank = new Bank("Junaid" , "0782");
         bank.showMenu();
     }
 }
